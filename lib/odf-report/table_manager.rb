55307b365b2553b3b8cd118a84452ac7d9b586cc
victerry.so <victerry.so@gmail.com>
2014-12-02 16:40:44 +1100

    works for weird docx format

--- lib/odf-report/table_manager.rb
+++ lib/odf-report/table_manager.rb
@@ -16,16 +16,27 @@ module ODFReport
 
       return unless filename == 'word/document.xml' # Go through word/document.xml
 
-      doc.xpath("//w:tbl//w:t").each do |txt| # Go through every table text
-        txt.inner_html.scan(/\{\{.*\((.*)\)\}\}/).each do |arg| # Scan to see if there's a condition
+      doc.xpath("//w:tr").each do |row|
+        row.xpath("descendant::*[w:t]//w:t").inner_html.scan(/\{\{.*\((.*)\)\}\}/).each do |arg| # Scan to see if there's a condition
           condition = $1.gsub(/&gt;/, '>').gsub(/&lt;/, '<') # Convert < and > signs
           if @calculator.evaluate condition # Evaluate Condition
-            txt.inner_html = txt.inner_html.gsub(/\s*\{\{.*\}\}/, '') # Remove Condition
+            row.xpath("*//w:t").each { |txt| txt.inner_html = txt.inner_html.gsub(/\{\{.*\}\}/, '').gsub(/\{\{.*/, '').gsub(/\}\}/, '') }
           else
-            txt.xpath("ancestor::*[w:tc]").remove # Remove Row
+            row.remove # Remove Row
           end
         end
       end
+
+      # doc.xpath("//w:tbl//w:t").each do |txt| # Go through every table text
+      #   txt.inner_html.scan(/\{\{.*\((.*)\)\}\}/).each do |arg| # Scan to see if there's a condition
+      #     condition = $1.gsub(/&gt;/, '>').gsub(/&lt;/, '<') # Convert < and > signs
+      #     if @calculator.evaluate condition # Evaluate Condition
+      #       txt.inner_html = txt.inner_html.gsub(/\s*\{\{.*\}\}/, '') # Remove Condition
+      #     else
+      #       txt.xpath("ancestor::*[w:tc]").remove # Remove Row
+      #     end
+      #   end
+      # end
 
     end
 
