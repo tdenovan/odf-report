1b0d061727495741ea3b25f136b16737d6edbf2d
victerry.so <victerry.so@gmail.com>
2015-02-23 18:37:08 +1100

    small change for negative numbers

--- lib/odf-report/text_field.rb
+++ lib/odf-report/text_field.rb
@@ -13,7 +13,7 @@ module ODFReport
       return unless /word\/document/ === filename
 
       name = "[#{@name.to_s.upcase}]"
-      value = @value.to_s
+      value = @value.to_s.gsub('-', '')
 
       doc.xpath("//w:default[@w:val='#{name}']").each do |field|
         field.xpath("ancestor::*[w:fldChar]/following-sibling::*/w:t").first.inner_html = value
